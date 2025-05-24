e155e4549151251c1b67235858f543d72da96199
Revers4 <stim_stim123321@mail.ru>
2025-05-24 17:01:18 +0500

    fix: url HighlightedText component

--- src/components/highLihgtedText/highLightedText.tsx
+++ src/components/highLihgtedText/highLightedText.tsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 
 interface HighlightedTextProps {
   text: string;
-  url?: string
 }
 
-const HighlightedText: React.FC<HighlightedTextProps> = ({ text, url }) => {
-  const regex = /(\*([^*]+)\*)|(_([^_]+)_)/g;
+const HighlightedText: React.FC<HighlightedTextProps> = ({ text }) => {
+  const regex = /(\*([^*]+)\*)|_([^_]+)_=>([^<]+)<=/g;
 
   const parts: (string | JSX.Element)[] = [];
   let lastIndex = 0;
@@ -21,26 +20,24 @@ const HighlightedText: React.FC<HighlightedTextProps> = ({ text, url }) => {
       parts.push(text.slice(lastIndex, start));
     }
 
+    // *жирный*
     if (match[1]) {
       parts.push(
         <span key={start} className="font-bold">
           {match[2]}
         </span>
       );
-    } else if (match[3]) {
-      if (url) {
-        parts.push(
-          <Link to={url} key={start} className="underline">
-            {match[4]}
-          </Link>
-        );
-      } else {
-        parts.push(
-          <span key={start} className="underline">
-            {match[4]}
-          </span>
-        );
-      }
+    }
+
+    // _текст_=>ссылка<=
+    if (match[3]) {
+      const underlineText = match[3];
+      const link = match[4];
+      parts.push(
+        <Link to={link} key={start} className="underline text-blue-600 hover:underline">
+          {underlineText}
+        </Link>
+      );
     }
 
     lastIndex = end;
--- src/components/pages/knowledge/Introduction.tsx
+++ src/components/pages/knowledge/Introduction.tsx
@@ -1,11 +1,10 @@
-import { NavLink } from "react-router-dom";
 import HighlightedText from "../../highLihgtedText/highLightedText";
 
 export default function Introduction() {
     return (
         <article className="">
             <h1>Hack Frontend - Платформа для подготовки к фронтенд собеседованиям</h1>
-            <HighlightedText text="Добро пожаловать в *Hack Frontend* — ваш надежный помощник в _подготовке к собеседованиям_!"/>
+            <HighlightedText text="Добро пожаловать в *Hack Frontend* — ваш надежный помощник в _подготовке к собеседованиям_=>../preparation-guide<=!"/>
         </article>
 
     );
